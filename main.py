0c03fd4c30cbfaddd4a3716619a236f61eb53a2a
Henry Ning <henryhuning@gmail.com>
2021-01-16 11:21:09 +0000

    71. Challenge 3 - Checking if the Player has Won 3min

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
+#Step 3
+
 import random
-
-#Step 1 
-
 word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-
-chosen_word = word_list[random.randint(0,len(word_list)-1)]
-print(chosen_word)
-
+#Create blanks
 display = []
-for letter in chosen_word:
-  display += "_"
-print(display)
-  
+for _ in range(word_length):
+    display += "_"
 
-guess = input("Guess a letter: ").lower()
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+while "_" in display:
+  guess = input("Guess a letter: ").lower()
 
-for position in range(len(chosen_word)):
-  letter = chosen_word[position]
-  if letter == guess:
-    display[position] = letter
+#Check guessed letter
+  for position in range(word_length):
+      letter = chosen_word[position]
+      print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+      if letter == guess:
+          display[position] = letter
 
-print(display)
+
+  print(display)
